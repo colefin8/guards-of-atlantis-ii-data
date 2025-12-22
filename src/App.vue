83a1e2349e58b35ce9bf79bf95841938b5ccd1b2
cole.fin8 <cole.finlayson@awardco.com>
2025-12-22 16:13:45 -0700

    forgot file

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ const router = useRouter()
 
 <template>
   <div>
-    <header class="flex bg-sky-900 text-white items-center justify-center p-3 flex-col cursor-pointer" @click="router.currentRoute.value.path !== '/' && router.push('/')">
+    <header class="flex bg-sky-900 text-white items-center justify-center p-3 flex-col cursor-pointer"
+      @click="router.currentRoute.value.path !== '/' && router.push('/')">
       <h1>GoA II Character Reference</h1>
       <nav v-if="router.currentRoute.value.path !== '/'">
         <router-link to="/">← Back to Characters</router-link>
